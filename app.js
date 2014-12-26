cddfdb080ff6e97a8e11853a268f2d0edae29a4b
James <james.a.forbes@gmail.com>
2014-12-26 14:59:55 +1100

    Get files works

--- app.js
+++ app.js
@@ -30,7 +30,9 @@ console.log("Reload the page with the following query params in the URL, auth_do
 function logged_in(auth_token){
   console.log(auth_token)
 
-  request({ url: 'https://api.dphoto.com/files', headers: { 'API-Version': '2.0', 'Auth-Token': auth_token }, json: true}).then(function(){
-    console.log(arguments)
+  request({ url: 'https://api.dphoto.com/files', headers: { 'API-Version': '2.0', 'Auth-Token': auth_token }, json: true})
+  .then(R.path('body.result'))
+  .then(function(files){
+    console.log(files)
   })
 }
